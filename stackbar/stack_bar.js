4b93cbe982fcef4d9457dc0c149811635473637f
Hiep Vo <hiep.vo@elsanow.io>
2021-03-15 11:59:30 -0500

    ble

--- stackbar/stack_bar.js
+++ stackbar/stack_bar.js
@@ -69,7 +69,7 @@ var barChartData = {
 window.onload = function() {
     var ctx = document.getElementById('canvas').getContext('2d');
     window.myBar = new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: barChartData,
         options: {
             title: {
